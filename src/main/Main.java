156c2ca2c822b268ff876031de41ad4f63f2a35c
Kilian Glaus <kilian.glaus@hotmail.com>
2024-04-27 12:32:37 +0200

    :fire: Remove code

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,13 +7,5 @@ public class Main {
 
         System.out.println("Hallo Welt");
 
-        WeatherStation weatherStation = new WeatherStation();
-        System.out.println("Temperature: " + weatherStation.getTemperature());
-        System.out.println("Luftdruck: " + weatherStation.getPressure());
-        System.out.println("Luftfeuchtigkeit: " + weatherStation.getHumidity());
-        weatherStation.getSME280Data();
-        System.out.println("\nTemperature: " + weatherStation.getTemperature());
-        System.out.println("Luftdruck: " + weatherStation.getPressure());
-        System.out.println("Luftfeuchtigkeit: " + weatherStation.getHumidity());
     }
 }
